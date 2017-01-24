e3c3a0962cee8c00fb6ffd9ee1aeff2095ed2d0a
MariaJoseDiaz <mjdiaz9@uc.cl>
2017-01-24 14:54:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,30 @@
-function setup() {
-  createCanvas(800, 800);
+ 
+   function drawEllipse(myWidht, myheight, r, g, b) {
+    fill(r, g, b);
+    noStroke()
+    ellipse(random(0, width), random(0, height), myWidht, myheight);
+    frameRate(7)
+  }
+  
+  function setup() {
+  createCanvas(400, 400)
 }
 
 function draw() {
-  if (mousePressed == true){
+  background(237, 34, 93);
+  fill(0);
 
-    background(255, 0, 0);
-    
-    if (mouseButton == LEFT){
-      
-      ellipse (400,400,200);
-    
-    }
-    
-    if (mouseButton == RIGHT){
-      
-      rect (200,200,400,400);
-    }
-    
-
-} else {
-
-  background(255, 255, 255);
+    if (mouseButton == LEFT === true){
+       drawEllipse(45, 45, 45, 168, 143);
+       
+  // } else { (mouseButton == RIGHT === false)
+      //   drawEllipse(130, 30, 147, 168, 45);
+   
+ //  if (false === (237, 34, 93) ){
+   //   drawEllipse(130, 30, 147, 168, 45);
+  // }
+   
+   
+ //    fill(255)
 }
-}
+  }
