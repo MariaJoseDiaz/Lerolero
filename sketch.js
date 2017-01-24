316c39c52a8788452a1cdb49656315d377b9e855
MariaJoseDiaz <mjdiaz9@uc.cl>
2017-01-24 12:49:50 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
- var touchStarted = 0
+ /* var touchStarted = 0
  
    function drawEllipse(myWidht, myheight, r, g, b) {
     fill(r, g, b);
@@ -35,8 +35,6 @@ function touch (){
 }
   }
 
-/*
-
  var touchStarted
  
    function drawEllipse(myWidht, myheight, r, g, b) {
@@ -63,6 +61,38 @@ function draw() {
      fill(255)
 }
   }
+  
+  */
+
+ 
+   function drawEllipse(myWidht, myheight, r, g, b) {
+    fill(r, g, b);
+    noStroke()
+    ellipse(random(0, width), random(0, height), myWidht, myheight);
+    frameRate(7)
+  }
+  
+  function setup() {
+  createCanvas(400, 400)
+}
+
+function draw() {
+  background(237, 34, 93);
+  fill(0);
+
+    if (touchStarted === true){
+       drawEllipse(45, 45, 45, 168, 143);
+       
+   } else { (touchStarted === false)
+      //   drawEllipse(130, 30, 147, 168, 45);
+   
+ //  if (false === (237, 34, 93) ){
+   //   drawEllipse(130, 30, 147, 168, 45);
+  // }
+   
+   
+     fill(255)
+}
 
 
 
