c5e50ac1efa96a15343e5ebeba309d8b7de0e829
MariaJoseDiaz <mjdiaz9@uc.cl>
2017-02-13 12:54:02 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,27 @@
-var cnv;
-var d;
-var g;
-function setup() {
-  cnv = createCanvas(100, 100);
-  cnv.touchStarted("#e25a93"); // attach listener for
-                                // canvas click only
-  d = 10;
-  g = 100;
+var bgcolor;
+
+  var x = 100;
+  var y = 100;
+  var rect_widht = 200;
+  var rect_height = 150;
+  
+function setup () {
+  createCanvas(windowWidth, windowHeight);
+  bgcolor = color(200);
+ // createButton("Change Background");
 }
 
-function draw() {
-  background(g);
-  ellipse(width/2, height/2, d, d);
+function touches () {
+  //if touches[x, y, rect_widht, rect_height]
+  if (mouseX >= x && mouseX <= x + rect_widht && mouseY >= x && mouseY <= x + rect_height)
+  bgcolor = color(random(0, 170), random(0, 170), random(0, 170));
+}
+
+function draw () {
+  background(bgcolor);
+  fill(255, 0, 0);
+  rect(x, y, rect_widht, rect_height);
+  fill(255);
+  textSize(60);
+  text('Click on background of canvas.', 100, 100);
 }
